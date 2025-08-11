ba3ea42b88b6f9623db6f239447c3a9290c8ea79
Anass Lamouri <anasslamouri@icloud.com>
2025-08-11 16:46:23 +0100

    display result in js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const resultEl = document.getElementById('result')
 
 function updateResults() {
 const convertedWeight = inputEl.value * 2.205
-
+resultEl.textContent = convertedWeight.toFixed(2)
 }
 
 inputEl.addEventListener('input', updateResults)
